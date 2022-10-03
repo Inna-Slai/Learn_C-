83b0be8e4da1a91d6f04105e5a8604f61303ebe7
InnaS <slaikovskaya@mail.ru>
2022-10-03 13:43:43 +0700

    Выполнили задание Ex002

--- Ex002/Program.cs
+++ Ex002/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("111 Hello, World!");
+﻿Console.WriteLine ("Введите Ваше имя");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);

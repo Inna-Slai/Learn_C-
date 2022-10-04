55cfd922f20f3a9351ec6ccc92351bc86ef540f8
InnaS <slaikovskaya@mail.ru>
2022-10-04 07:51:25 +0700

    Добавили в алгоритм суммы Рандомные числа и вывод их на экран

--- Ex003_Sum/Program.cs
+++ Ex003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberB = 3;
-int numberC = 4;
+﻿int numberB = new Random() .Next(1, 10); // 1,2,3... 9
+Console.WriteLine(numberB);
+int numberC = new Random() .Next(1, 10);
+Console.WriteLine(numberC);
 int result = numberB + numberC;
 Console.WriteLine(result);

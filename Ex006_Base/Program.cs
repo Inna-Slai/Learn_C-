8418a5e56baaf887aaa6e08bd40a8b39758ceb56
InnaS <slaikovskaya@mail.ru>
2022-10-04 09:59:09 +0700

    Создали алгоритм нахождения максимального числа

---
+++ Ex006_Base/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 5;
+int b = 8;
+int c = 1;
+int d = 3;
+int e = 15;
+int max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+if(d > max) max = d;
+if(e > max) max = e;
+
+Console.Write("Максимальное значение: ");
+Console.Write (max);
